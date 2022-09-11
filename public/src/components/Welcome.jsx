c140e30d4fb96ee54f1a4caf750ff19af3ce27ab
Michealajit <michealjit1@gmail.com>
2022-09-12 00:31:26 +0530

    2

--- public/src/components/Welcome.jsx
+++ public/src/components/Welcome.jsx
@@ -11,7 +11,7 @@ export default function Welcome() {
             ).username
           );
     }
-    fetchData();
+     userName&& fetchData();
   }, []);
  
   return (
--- server/routes/messageRoute.js
+++
@@ -1,8 +0,0 @@
-const { getAllmsg, addMsg } = require("../controllers/messageControllers");
-
-
-const router = require("express").Router();
-
-router.post("/getmsg",getAllmsg);
-router.post("/addmsg",addMsg);
-module.exports = router;
