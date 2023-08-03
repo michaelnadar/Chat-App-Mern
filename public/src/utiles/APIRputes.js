8b0df59303098f2b3a6e9a4e21767bbf894b1a52
Michealajit <michealajit1@gmail.com>
2023-08-03 16:11:42 +0000

    server changed

--- public/src/utiles/APIRputes.js
+++ public/src/utiles/APIRputes.js
@@ -1,4 +1,4 @@
-  export  const host = "https://chatapp-ajit.herokuapp.com";
+  export  const host = "https://api-xcn4.onrender.com";
   export const registerRoute = `${host}/api/auth/register`;
   export const loginRoute = `${host}/api/auth/login`;
   export const setAvatarRoute = `${host}/api/auth/setAvatar`;
